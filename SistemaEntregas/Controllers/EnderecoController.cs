7008b7508ca45e2051de56fd86816d17393056cd
User <User@DESKTOP-GN27T58>
2018-03-20 13:13:20 -0300

    Metodos no endereco

--- SistemaEntregas/Controllers/EnderecoController.cs
+++ SistemaEntregas/Controllers/EnderecoController.cs
@@ -8,5 +8,29 @@ namespace Controllers
 {
     public class EnderecoController
     {
+
+        static List<Endereco> Enderecos = new List<Endereco>();
+        static int ultimoID = 0;
+
+        public void SalvarEndereco(Endereco endereco )
+        {
+            int id = ultimoID + 1;
+            ultimoID = id;
+
+            endereco.EnderecoID = id;
+            Enderecos.Add(endereco);
+        }
+
+        public Endereco PesquisarPorID(int idEndereco)
+        {
+            var c = from x in Enderecos
+                    where x.EnderecoID.Equals(idEndereco)
+                    select x;
+
+            if (c != null)
+                return c.FirstOrDefault();
+            else
+                return null;
+        }
     }
 }
